3033ab1d56ebcc9f984b091a5ce61ac10d1e2f11
Nathalia Kasman <34670412+natkasman@users.noreply.github.com>
2018-02-12 10:41:00 -0800

    Create classexercise_3moresounds.js

---
+++ classexercise_3moresounds.js
@@ -0,0 +1,34 @@
+var mySound;
+var mySound2;
+var mySound3;
+
+function preload() {
+  mySound = loadSound('assets/SOUND_EFFECT_Punch[Mp3Converter.net].mp3');
+	mySound2 = loadSound('assets/DamnSon.mp3');
+	mySound3 = loadSound('assets/Splash.mp3')
+}
+
+function setup() { 
+  mySound.setVolume(0.5);
+  mySound.play();
+  createCanvas(100, 100);
+}
+
+function draw() {
+  if (keyIsPressed) {
+    background(240);
+  } else {
+    background(120);
+  }
+}
+
+function keyPressed() {
+	print(key);
+	if (key == 'P') {
+  mySound.play();
+	} else if (key == 'G') {
+	mySound2.play();
+	} if (key == 'B') {
+	mySound3.play();
+	}
+}
