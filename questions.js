f2ea38cc470470f35f4edd03d1f8b1b5c4bb29bd
Nathalia Kasman <34670412+natkasman@users.noreply.github.com>
2018-02-19 01:07:27 -0800

    Create questions.js

---
+++ questions.js
@@ -0,0 +1,16 @@
+1. What is an object?
+Container with variables
+
+2. How do you access a property of an object?
+With syntaxes (dot)
+
+3. What is the index of the first item in an array?
+0 (Zero)
+
+4. What syntax can you use to get the third item in an array?
+nums[2] 
+nums as the name of the variable, the number in the square bracket 
+calls the third item in the array.
+
+5. How do you use a variable and a loop to get every item in an array?
+var a = 0; a < 4; a++
